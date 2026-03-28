f6763cb70b6c8fce9a9b1ad767a0e38e3c43e6c8
Redheris <solomonov010@mail.ru>
2026-03-28 13:16:49 +0400

    Bump dependency versions for 26.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ stonecutter {
     create(rootProject) {
         // See https://stonecutter.kikugie.dev/wiki/start/#choosing-minecraft-versions
         versions("1.21.8", "1.21.11").buildscript("build.fabric_remap.gradle.kts")
-        version("26.1", "26.1-rc-2").buildscript("build.fabric.gradle.kts")
+        version("26.1", "26.1").buildscript("build.fabric.gradle.kts")
         vcsVersion = "1.21.8"
     }
 }
